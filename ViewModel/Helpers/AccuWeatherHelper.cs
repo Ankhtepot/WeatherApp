306359e29d74d99232aa23a8bb84cfd7e2675581
Ankhtepot <vessago@napismi.cz>
2020-01-25 17:14:36 +0100

    Initial commit

---
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        // location example http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=NFlqJPZl7TSF5O8fagyUqVhrHlydBmgK&q=Brno
+        // current weather example http://dataservice.accuweather.com/currentconditions/v1/347629?apikey=NFlqJPZl7TSF5O8fagyUqVhrHlydBmgK
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "NFlqJPZl7TSF5O8fagyUqVhrHlydBmgK";
+
+        public async static Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<WeatherConditions> GetCurrentConditions(string cityKey)
+        {
+            WeatherConditions currentConditions = new WeatherConditions();
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                currentConditions = JsonConvert
+                    .DeserializeObject<List<WeatherConditions>>(json)
+                    .FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+    }
+}
---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Commands;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+        public string Query {
+            get { return query; }
+            set 
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        public ObservableCollection<City> Cities { get; set; }
+
+        private WeatherConditions currentConditions;
+        public WeatherConditions CurrentConditions {
+            get { return currentConditions; }
+            set 
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+        public City SelectedCity {
+            get { return selectedCity; }
+            set {
+                selectedCity = value;
+                if (selectedCity != null)
+                {
+                    OnPropertyChanged("SelectedCity");
+                    GetCurrentCondition(); 
+                }
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject())) 
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentConditions = new WeatherConditions
+                {
+                    WeatherText = "Partly Cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = "21"
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+
+            Cities = new ObservableCollection<City>();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void GetCurrentCondition()
+        {
+            Query = string.Empty;
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(selectedCity.Key);
+            Cities.Clear();
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            cities.ForEach(city => Cities.Add(city));
+           
+        }
+    }
+}
